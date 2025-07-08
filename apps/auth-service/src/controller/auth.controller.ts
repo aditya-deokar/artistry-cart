082ddd06683d25c5b93bf99a321118fb480d5fc9
adiya-deokar <adityadeokar80@gmail.com>
2025-07-08 23:43:27 +0530

    feat: add Prisma schema for MongoDB with images and users models

---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,46 @@
+// Register a new user
+
+import { Request, Response, NextFunction } from "express";
+import {
+  checkOTPRestrictions,
+  sendOTP,
+  trackOTPRequests,
+  validationRegistrationData,
+} from "../utils/auth.helper";
+import prisma from "../../../../packages/libs/prisma";
+import { ValidationError } from "../../../../packages/error-handler";
+
+export const userRegistration = async (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    validationRegistrationData(req.body, "user");
+
+    const { name, email } = req.body;
+
+    const existingUser = await prisma.users.findUnique({
+      where: email,
+    });
+
+    if (existingUser) {
+      return next(new ValidationError("User already exists with this email!"));
+    }
+
+    await checkOTPRestrictions(email, next);
+
+    await trackOTPRequests(email, next);
+
+    await sendOTP(email, name, "user-activation-mail");
+
+    res.status(200).json({
+      message: "OTP sent to email. Please verify your account.",
+    });
+
+  } catch (error) {
+    // console.log(error);
+    return next(error);
+  }
+};
---
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -0,0 +1,60 @@
+import crypto from 'crypto';
+import { ValidationError } from '../../../../packages/error-handler';
+import { NextFunction } from 'express';
+import redis from '../../../../packages/libs/redis';
+import { sendEmail } from './sendMail';
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+export const validationRegistrationData =(data:any, userType:'user' | 'seller') => {
+    const { name , email, password, phone_number, country } =data;
+
+    if(!name || !email || !password || (userType === 'seller' && (!phone_number || !country)) ){
+        throw new ValidationError(`Missing Required fields!`)
+    }
+
+    if(!emailRegex.test(email)){
+        throw new ValidationError("Invalid email format!");
+    }
+
+}
+
+
+export const checkOTPRestrictions = async(email:string, next:NextFunction) => {
+    if(await redis.get(`otp-lock:${email}`)){
+        return next( new ValidationError("Account locked due to multiple failed attempts! Try again after 30 minutes"));
+    }
+
+    if(await redis.get(`otp_spam_lock:${email}`)){
+        return next(new ValidationError("Too many OTP requests! Please wait 1 hour before requesting again."));
+    }
+
+    if(await redis.get(`otp_cooldown:${email}`)){
+        return next(new ValidationError("Please wait 1 minute before requesting a new OTP!"));
+    }
+}
+
+export const sendOTP = async(name:string, email:string, template:string) => {
+    const otp = crypto.randomInt(1000, 9999).toString();
+
+    await sendEmail(email, 'Verify Your Email', template, {name, otp});
+
+    // Redis 
+    await redis.set(`otp:${email}`, otp, 'EX', 300);
+    await redis.set(`otp_cooldown:${email}`, 'true', 'EX', 60);
+
+}
+
+
+export const trackOTPRequests = async (email:string, next:NextFunction)=>{
+    const otpRequestKey = `otp_request_count:${email}`;
+    let otpRequests = parseInt((await redis.get(otpRequestKey)) || '0');
+
+    if(otpRequests >= 2){
+        await redis.set(`otp_span_lock:${email}`, "locked" , "EX", 3600);
+        return next (new ValidationError("Too many OTP requests. Please wait 1 hour before requesting again."))
+    }
+
+    await redis.set(otpRequestKey, otpRequests + 1, 'EX', 3600)
+}
